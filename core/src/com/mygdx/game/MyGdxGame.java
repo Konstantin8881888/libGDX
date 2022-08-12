d20182214b5428e7f1591539e2970a9f4136aeae
Konstantin <kamagoroff@gmail.com>
2022-08-12 23:15:44 +0300

    Back

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -1,8 +1,6 @@
 package com.mygdx.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
@@ -11,31 +9,21 @@ public class MyGdxGame extends ApplicationAdapter
 {
 	SpriteBatch batch;
 	Texture img;
-	int clk;
 	
 	@Override
 	public void create ()
 	{
 		batch = new SpriteBatch();
-		img = new Texture("win.png");
+		img = new Texture("badlogic.jpg");
 	}
 
 	@Override
 	public void render ()
 	{
-		ScreenUtils.clear(1, 0, 0.5f, 1);
-
-		float x = Gdx.input.getX() - img.getWidth()/2;
-		float y = Gdx.graphics.getHeight()-Gdx.input.getY() - img.getHeight()/2;
-
-		if(Gdx.input.isButtonJustPressed(Input.Buttons.LEFT)) clk++;
-		if(Gdx.input.isButtonJustPressed(Input.Buttons.RIGHT)) clk--;
-
-		Gdx.graphics.setTitle("Сделан(о) " + clk + " клик(ов)");
+		ScreenUtils.clear(1, 0, 0, 1);
 
 		batch.begin();
-//		batch.draw(img, 0, 0);
-		batch.draw(img, x, y);
+		batch.draw(img, 0, 0);
 		batch.end();
 	}
 	
