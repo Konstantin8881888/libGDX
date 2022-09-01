7af9d2140050be59cf1329060ce652d34235d326
Konstantin <kamagoroff@gmail.com>
2022-09-01 04:20:34 +0300

    Сделано задание второго урока.

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -3,21 +3,24 @@ package com.mygdx.game;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
-import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 public class MyGdxGame extends ApplicationAdapter
 {
 	SpriteBatch batch;
-	Texture img;
 	int clk;
-	
+	Anim animation;
+	boolean dir = true;
+	int frameSetX = 0;
+
 	@Override
 	public void create ()
 	{
 		batch = new SpriteBatch();
-		img = new Texture("win.png");
+		animation = new Anim("animation.png", 7, 4, Animation.PlayMode.LOOP);
+
 	}
 
 	@Override
@@ -25,17 +28,57 @@ public class MyGdxGame extends ApplicationAdapter
 	{
 		ScreenUtils.clear(1, 0, 0.5f, 1);
 
-		float x = Gdx.input.getX() - img.getWidth()/2;
-		float y = Gdx.graphics.getHeight()-Gdx.input.getY() - img.getHeight()/2;
+		animation.setTime(Gdx.graphics.getDeltaTime());
 
-		if(Gdx.input.isButtonJustPressed(Input.Buttons.LEFT)) clk++;
-		if(Gdx.input.isButtonJustPressed(Input.Buttons.RIGHT)) clk--;
+		float x = Gdx.input.getX() - animation.getFrame().getRegionWidth()/2;
+		float y = Gdx.graphics.getHeight()-Gdx.input.getY() - animation.getFrame().getRegionHeight()/2;
 
-		Gdx.graphics.setTitle("Сделан(о) " + clk + " клик(ов)");
+//		if(Gdx.input.isButtonJustPressed(Input.Buttons.LEFT)) clk++;
+//		if(Gdx.input.isButtonJustPressed(Input.Buttons.RIGHT)) clk--;
+
+//		Gdx.graphics.setTitle("Сделан(о) " + clk + " клик(ов)");
+
+		if (Gdx.input.isKeyJustPressed(Input.Keys.R))
+		{
+			dir = false;
+		}
+		if (Gdx.input.isKeyJustPressed(Input.Keys.L))
+		{
+			dir = true;
+		}
+
+		if (frameSetX + 130 >= Gdx.graphics.getWidth())
+		{
+			dir = false;
+		}
+		if (frameSetX <= 0)
+		{
+			dir = true;
+		}
+
+
+		if (!animation.getFrame().isFlipX() && !dir)
+		{
+			animation.getFrame().flip(true, false);
+		}
+		if (animation.getFrame().isFlipX() && dir)
+		{
+			animation.getFrame().flip(true, false);
+		}
+
+
+		if (dir)
+		{
+			frameSetX += 3;
+		}
+		else
+		{
+			frameSetX -= 3;
+		}
 
 		batch.begin();
-//		batch.draw(img, 0, 0);
-		batch.draw(img, x, y);
+		batch.draw(animation.getFrame(), frameSetX, 0);
+//		batch.draw(img, x, y);
 		batch.end();
 	}
 	
@@ -43,6 +86,6 @@ public class MyGdxGame extends ApplicationAdapter
 	public void dispose ()
 	{
 		batch.dispose();
-		img.dispose();
+		animation.dispose();
 	}
 }
